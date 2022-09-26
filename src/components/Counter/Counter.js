d1966d8cfd3d45daca0f3f681b9a6b12b953a889
Anastasiia <106449970+Anastasiia1002@users.noreply.github.com>
2022-09-26 11:39:21 +0300

    edit

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -6,25 +6,11 @@ import Section from './Controls/Section/Section';
 import Notification from './Controls/Notification/Notification';
 
 export default function Counter() {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const onLeaveFeedback = name => {
-    // this.setState(prevState => ({
-    //   [name]: prevState[name] + 1,
-    // }));
-
-    // setGood(el.target.value + 1);
-    // setGood(good + 1);
-    // setNeutral(neutral + 1);
-    // setBad(bad + 1);
     switch (name) {
       case 'good':
         setGood(prev => prev + 1);
@@ -44,26 +30,20 @@ export default function Counter() {
   };
 
   const countTotalFeedback = () => {
-    // const { good, neutral, bad } = this.state;
-    // const initValue = 0;
-    // const result = Object.values(this.state).reduce(
-    //   (previousValue, currentValue) => previousValue + currentValue,
-    //   initValue
-    // );
     const result = good + neutral + bad;
     return result;
   };
 
   const countPositiveFeedbackPercentage = () => {
     const total = countTotalFeedback();
-    // const { good } = this.state;
+
     const percentage = Math.round((good / total) * 100);
 
     return good ? percentage : 0;
   };
 
   const Total = countTotalFeedback();
-  //   const options = Object.keys(this.state);
+
   return (
     <div>
       <div className="Counter">
